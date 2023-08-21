95be6838d21d4317f1aa2b9c9fb3cf29812355f4
Quasark <q@quasark.xyz>
2023-08-21 14:36:11 -0700

    forms are going

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,41 @@ import axios from 'axios';
 const API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
 
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchQuery: '',
+    }
+  }
 
-  return (
-    <>
-      <h1>Day6, Yay</h1>
-      {console.log(API_KEY)}
-    </>
-  )
+  handleForm = (e) => {
+    e.preventDefault();
+    console.log('City name provided', this.state.searchQuery);
+    console.log('API Key: ', API_KEY)
+
+  }
+
+
+  handleSubmit= (e) => {
+    const newQuery = e.target.value;
+    this.setState({searchQuery: newQuery})
+  }
+
+  render () {
+    return (
+      <>
+        <h1>Day6, v2</h1>
+        <form onClick={this.handleForm}>
+          <input placeholder="Enter City Name" name="city" type="text" onChange={this.handleSubmit} />
+            <button type='submit' >
+              Explore!
+              {/* <Link to="/search">Search!</Link> */}
+            </button>
+        </form>
+      </>
+      )
+    }
 }
 
 export default App
