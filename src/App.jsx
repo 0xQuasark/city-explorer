247d10a4b4374973f2847a1b33c07bbdc352e9f3
Quasark <q@quasark.xyz>
2023-08-21 12:56:15 -0700

    netlify again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-// import axios from 'axios';
+import axios from 'axios';
 
 // Vites way of loading files from a .env file -> requires "VITE_" to be used at the beginning of your key
 const API_KEY = import.meta.env.VITE_LOCATIONIQ_API_KEY;
